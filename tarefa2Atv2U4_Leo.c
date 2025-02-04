9af23c9ee8b1a75b8ba258e1cae1f3e7f46bdc38
Leo-Luz-code <leonardoluz768@gmail.com>
2025-02-04 18:37:09 -0300

    feat: add led and button connections

--- tarefa2Atv2U4_Leo.c
+++ tarefa2Atv2U4_Leo.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/timer.h"
+#include "hardware/gpio.h"
 
+// Definindo os pinos dos leds e do botão
+#define GREEN_LED 11
+#define RED_LED 12
+#define BLUE_LED 13
+#define BUTTON 5
 
+// Função para inicializar os leds e o botão
+void initialize_leds_and_button()
+{
+    gpio_init(BLUE_LED);
+    gpio_init(RED_LED);
+    gpio_init(GREEN_LED);
+    gpio_init(BUTTON);
+
+    gpio_set_dir(BLUE_LED, GPIO_OUT);
+    gpio_set_dir(RED_LED, GPIO_OUT);
+    gpio_set_dir(GREEN_LED, GPIO_OUT);
+    gpio_set_dir(BUTTON, GPIO_IN);
+    gpio_pull_up(BUTTON);
+}
 
 int main()
 {
     stdio_init_all();
 
-    while (true) {
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
